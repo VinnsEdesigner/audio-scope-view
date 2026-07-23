61ab691b1753e25f433ac22111d1f0e5d73b34e5
VinnsEdesigner <VinnsEdesigner@users.noreply.github.com>
2026-07-23 08:00:45 +0000

    refactor: Remove sidebar, add hamburger menu navigation
    - Remove sidebar from oscilloscope page
    - Add hamburger icon that pops out menu from icon
    - Move all navigation into hamburger dropdown menu
    - End-to-end layout for oscilloscope page
    - Clean up unused scope component files

--- apps/vyzorWeb/src/components/scope/index.ts
+++ apps/vyzorWeb/src/components/scope/index.ts
@@ -1,6 +1,1 @@
-export { ScopePage } from "./scope-page";
-export { ScopeSidebar } from "./scope-sidebar";
-export { ScopeTopBar } from "./scope-top-bar";
-export { ScopeCanvas } from "./scope-canvas";
-export { ScopeControlPanel } from "./scope-control-panel";
-export { ScopeBottomBar } from "./scope-bottom-bar";
+
